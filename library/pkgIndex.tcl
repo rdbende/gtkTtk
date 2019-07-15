00a44fea73822114aa7ef585233805798838c4e5
géballin <macniaque@free.fr>
2019-07-15 13:27:13 +0200

    Upgrade version to 0.4

--- library/pkgIndex.tcl
+++ library/pkgIndex.tcl
@@ -1,2 +1,2 @@
-package ifneeded ttk::theme::tilegtk 0.3 \
-  [list load [file join $dir libtilegtk0.3[info sharedlibextension]] tilegtk]
+package ifneeded ttk::theme::tilegtk 0.4 \
+  [list load [file join $dir libtilegtk0.4[info sharedlibextension]] tilegtk]
